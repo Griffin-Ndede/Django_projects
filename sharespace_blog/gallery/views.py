741377a0ebb26bf32f3dcb057da8fb3da6c7e6fe
Griffin <omondigriffin4@gmail.com>
2024-05-27 06:18:35 +0300

    creates a products model

---
+++ sharespace_blog/gallery/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from .models import Product
+from django.http import HttpResponse
+from django.shortcuts import render, redirect, get_object_or_404
+
+def product_list(request):
+    products = Product.objects.all()
+    return render(request, 'products.html', {'products': products})
