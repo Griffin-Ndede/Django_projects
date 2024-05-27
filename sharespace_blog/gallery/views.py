57cb56b37359072d27b654c704c155ae3860ae33
Griffin <omondigriffin4@gmail.com>
2024-05-27 16:37:30 +0300

    displays details of the blog posts

--- sharespace_blog/gallery/views.py
+++ sharespace_blog/gallery/views.py
@@ -4,18 +4,19 @@ from django.http import HttpResponse
 from django.shortcuts import render, redirect, get_object_or_404
 from .form import ProductForm
 
+
 def product_list(request):
     products = Product.objects.all()
     return render(request, 'products.html', {'products': products})
 
 def product_detail(request, id):
-    products = get_object_or_404(id=id)
-    return render (request, "productdetails.html" , {"product": products})
+    product = get_object_or_404(Product, id=id)
+    return render(request, "productdetails.html", {"product": product})
 
 def edit_product(request, id):
     product = get_object_or_404(Product, id=id)
     if request.method == "POST":
-        form = ProductForm(request.POST, instance = product)
+        form = ProductForm(request.POST, instance=product)
         if form.is_valid():
             form.save()
             return redirect("product_list")
@@ -24,12 +25,11 @@ def edit_product(request, id):
     return render(request, "edit.html", {"form": form})
 
 def delete_product(request, id):
-    product = get_object_or_404(Product,id=id)
-    if request.method == " POST":
+    product = get_object_or_404(Product, id=id)
+    if request.method == "POST":
         product.delete()
         return redirect("product_list")
     return render(request, "delete.html", {"product": product})
-
 
 def home(request):
     return HttpResponse("Hello world")
