6810b52ee4824c89d9827c444fe8115fcb876a9c
Griffin <omondigriffin4@gmail.com>
2024-05-27 12:55:57 +0300

    creates super user

--- sharespace_blog/gallery/views.py
+++ sharespace_blog/gallery/views.py
@@ -6,3 +6,10 @@ from django.shortcuts import render, redirect, get_object_or_404
 def product_list(request):
     products = Product.objects.all()
     return render(request, 'products.html', {'products': products})
+
+def product_detail(request, id):
+    products = Product.objects.all()
+    return render (request, "productdetails.html" , {"product": products})
+
+def home(request):
+    return HttpResponse("Hello world")
