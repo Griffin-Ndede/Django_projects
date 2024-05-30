4f310e2710e6bc88128e71c421b01d2f4400a647
Griffin <omondigriffin4@gmail.com>
2024-05-30 11:22:20 +0300

    creates a user app

--- polling_site/polling_site/urls.py
+++ polling_site/polling_site/urls.py
@@ -16,8 +16,17 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from user import views as user_view
+from django.contrib.auth import views as auth
 
 urlpatterns = [
-    path("polls/", include("polls.urls")),
     path('admin/', admin.site.urls),
+
+    path("polls/", include("polls.urls")),
+    
+    # user related paths
+    path("user/", include("user.urls")),
+    path('login/', user_view.Login, name ='login'),
+    path('logout/', auth.LogoutView.as_view(template_name ='user/index.html'), name ='logout'),
+    path('register/', user_view.register, name ='register'),
 ]
